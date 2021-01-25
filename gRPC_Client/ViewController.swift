4146d9ec4ac1c87a71ae3a8a15580d7a5e816979
이우경(Woogyeong Lee) <rainyi@ncsoft.com>
2021-01-25 20:04:56 +0900

    -. gRPCManager 진행 중.

--- gRPC_Client/ViewController.swift
+++ gRPC_Client/ViewController.swift
@@ -15,42 +15,27 @@ import NIOHTTP1
 import NIOHPACK
 
 class ViewController: UIViewController, UITextFieldDelegate {
-
-    @IBOutlet weak var btnJoin: UIButton!
-    @IBOutlet weak var btnLeave: UIButton!
     
+    @IBOutlet weak var btnConnect: UIButton!
+    @IBOutlet weak var btnDisconnect: UIButton!
+    @IBOutlet weak var btnGetRoomList: UIButton!
+    @IBOutlet weak var btnJoin: UIButton!
+    @IBOutlet weak var btnCreate: UIButton!
+    @IBOutlet weak var btnLeave: UIButton!
+    @IBOutlet weak var btnSend: UIButton!
+    
+    @IBOutlet weak var tfHostName: UITextField!
+    @IBOutlet weak var tfPort: UITextField!
+    @IBOutlet weak var tfRoomID: UITextField!
     @IBOutlet weak var tfMessage: UITextField!
     
     var chatRoom: Com_Ncsoft_Aiss_Chat_Paige_V1_ChatRoom = Com_Ncsoft_Aiss_Chat_Paige_V1_ChatRoom()
-    var client: Com_Ncsoft_Aiss_Chat_Paige_V1_ChatRoomServiceClient?
-    var streamRequest = Com_Ncsoft_Aiss_Chat_Paige_V1_ChatStreamRequest()
-    var bdsCall: BidirectionalStreamingCall<Com_Ncsoft_Aiss_Chat_Paige_V1_ChatStreamRequest, Com_Ncsoft_Aiss_Chat_Paige_V1_ChatStreamResponse>?
     
     var grpcManager: gRPCManager = gRPCManager()
-    
-    lazy var configuration: GRPC.ClientConnection.Configuration = {
-        let hostName: String = "172.19.136.161"
-        let port: Int = 31105
-        
-        let configuration = ClientConnection.Configuration(
-            target: .hostAndPort(hostName, port),
-            eventLoopGroup: MultiThreadedEventLoopGroup(numberOfThreads: 1)
-        )
-        
-        return configuration
-    }()
-    
-    lazy var streamClient: Com_Ncsoft_Aiss_Chat_Paige_V1_ChatStreamServiceClientProtocol = {
-        let streamClient = Com_Ncsoft_Aiss_Chat_Paige_V1_ChatStreamServiceClient(channel: ClientConnection(configuration: configuration))
-        
-        return streamClient
-    }()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.btnJoin.isEnabled = false
-        self.tfMessage.isHidden = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -60,122 +45,195 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
     func commonInit() {
-#if true
-        if let response: Com_Ncsoft_Aiss_Chat_Paige_V1_GetAllChatRoomResponse = self.grpcManager.getChatRoomList() {
-            if response.chatRooms.count > 0 {
-                if let selectChatRoom: Com_Ncsoft_Aiss_Chat_Paige_V1_ChatRoom = response.chatRooms.first {
-                    self.chatRoom = selectChatRoom
-                    self.btnJoin.isEnabled = true
-                }
-            }
-        }
-#else
-        if let response: Com_Ncsoft_Aiss_Chat_Paige_V1_GetAllChatRoomResponse = self.getChatRoom() {
-            if response.chatRooms.count > 0 {
-                if let selectChatRoom: Com_Ncsoft_Aiss_Chat_Paige_V1_ChatRoom = response.chatRooms.first {
-                    self.chatRoom = selectChatRoom
-                    self.btnJoin.isEnabled = true
-                }
-            }
-        }
-#endif
+        self.initButtons()
+        self.makeBorderButton()
+        self.changeStateButton()
+        
+//        self.btnConnect.isEnabled = true
+//        self.btnDisconnect.isEnabled = false
+//        self.btnGetRoomList.isEnabled = false
+//        self.btnCreate.isEnabled = false
+//        self.btnJoin.isEnabled = false
+//        self.btnLeave.isEnabled = false
+//        self.btnSend.isEnabled = false
     }
     
-    func getChatRoom() -> Com_Ncsoft_Aiss_Chat_Paige_V1_GetAllChatRoomResponse? {
-        do {
-            self.client = Com_Ncsoft_Aiss_Chat_Paige_V1_ChatRoomServiceClient(channel: ClientConnection(configuration: self.configuration))
-            let request = Com_Ncsoft_Aiss_Chat_Paige_V1_GetAllChatRoomRequest()
-            
-            if let chatRoomServiceClient: Com_Ncsoft_Aiss_Chat_Paige_V1_ChatRoomServiceClient = self.client {
-                let response = try chatRoomServiceClient.getAll(request).response.wait()
-
-                print(response)
-                
-                return response
-            }
-        } catch {
-            print("ERROR\n\(error)")
-        }
-        
-        return nil
+    func initButtons() {
+        self.btnConnect.setBackgroundImage(self.colorGraphic(color: UIColor.gray), for: .disabled)
+        self.btnDisconnect.setBackgroundImage(self.colorGraphic(color: UIColor.gray), for: .disabled)
+        self.btnGetRoomList.setBackgroundImage(self.colorGraphic(color: UIColor.gray), for: .disabled)
+        self.btnJoin.setBackgroundImage(self.colorGraphic(color: UIColor.gray), for: .disabled)
+        self.btnCreate.setBackgroundImage(self.colorGraphic(color: UIColor.gray), for: .disabled)
+        self.btnLeave.setBackgroundImage(self.colorGraphic(color: UIColor.gray), for: .disabled)
+        self.btnSend.setBackgroundImage(self.colorGraphic(color: UIColor.gray), for: .disabled)
     }
     
-    func joinToChatRoom() {
-        self.streamRequest.chatRoomID = "asdfasdfkjaslkfjaslkfjslk" // self.chatRoom.id
-        self.streamRequest.command = .join
-        self.streamRequest.userID = "2"
-        self.streamRequest.userName = "레이니2"
+    func colorGraphic(color:UIColor) -> UIImage {
+        UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
         
-        self.bdsCall = self.streamClient.send(handler: { [weak self] (streamRequest) in
-            print(streamRequest)
-            
-            if let weakSelf = self {
-                DispatchQueue.main.async {
-                    if streamRequest.command == .chat {
-                        weakSelf.tfMessage.text = ""
-                        weakSelf.view.endEditing(true)
-                    } else {
-                        weakSelf.tfMessage.isHidden = false
-                    }
-                }
-            }
-        })
-        
-        if let call: BidirectionalStreamingCall<Com_Ncsoft_Aiss_Chat_Paige_V1_ChatStreamRequest, Com_Ncsoft_Aiss_Chat_Paige_V1_ChatStreamResponse>
-            = self.bdsCall {
-            call.sendMessage(streamRequest, compression: .deferToCallDefault, promise: .none)
+        if let context = UIGraphicsGetCurrentContext() {
+            context.setFillColor(color.cgColor)
+            context.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
         }
+        
+        let colorImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        if let image = colorImage {
+            return image
+        }
+        
+        return UIImage.init()
     }
     
-    func sendMessageToChatRoom(message: String) {
-        self.view.endEditing(true)
+    func makeBorderButton() {
+        self.btnConnect.layer.masksToBounds = true
+        self.btnConnect.layer.cornerRadius = 5
+        self.btnConnect.layer.borderWidth = 0.5
+        self.btnConnect.layer.borderColor = UIColor.black.cgColor
         
-        self.streamRequest.command = .chat
-        self.streamRequest.message = message
+        self.btnDisconnect.layer.masksToBounds = true
+        self.btnDisconnect.layer.cornerRadius = 5
+        self.btnDisconnect.layer.borderWidth = 0.5
+        self.btnDisconnect.layer.borderColor = UIColor.black.cgColor
         
-        if let call: BidirectionalStreamingCall<Com_Ncsoft_Aiss_Chat_Paige_V1_ChatStreamRequest, Com_Ncsoft_Aiss_Chat_Paige_V1_ChatStreamResponse> = self.bdsCall {
-            call.sendMessage(streamRequest, compression: .deferToCallDefault, promise: .none)
+        self.btnGetRoomList.layer.masksToBounds = true
+        self.btnGetRoomList.layer.cornerRadius = 5
+        self.btnGetRoomList.layer.borderWidth = 0.5
+        self.btnGetRoomList.layer.borderColor = UIColor.black.cgColor
+        
+        self.btnJoin.layer.masksToBounds = true
+        self.btnJoin.layer.cornerRadius = 5
+        self.btnJoin.layer.borderWidth = 0.5
+        self.btnJoin.layer.borderColor = UIColor.black.cgColor
+        
+        self.btnCreate.layer.masksToBounds = true
+        self.btnCreate.layer.cornerRadius = 5
+        self.btnCreate.layer.borderWidth = 0.5
+        self.btnCreate.layer.borderColor = UIColor.black.cgColor
+        
+        self.btnLeave.layer.masksToBounds = true
+        self.btnLeave.layer.cornerRadius = 5
+        self.btnLeave.layer.borderWidth = 0.5
+        self.btnLeave.layer.borderColor = UIColor.black.cgColor
+        
+        self.btnSend.layer.masksToBounds = true
+        self.btnSend.layer.cornerRadius = 5
+        self.btnSend.layer.borderWidth = 0.5
+        self.btnSend.layer.borderColor = UIColor.black.cgColor
+    }
+    
+    func changeStateButton() {
+        var state: ConnectivityState = .idle
+        
+        if let s: ConnectivityState = self.grpcManager.connection?.connectivity.state {
+            state = s
+        }
+        
+        if state == .idle || state == .shutdown {
+            self.btnConnect.isEnabled = true
+            self.btnDisconnect.isEnabled = false
+            self.btnGetRoomList.isEnabled = false
+            self.btnCreate.isEnabled = false
+            self.btnJoin.isEnabled = false
+            self.btnLeave.isEnabled = false
+            self.btnSend.isEnabled = false
+        } else if state == .ready {
+            self.btnConnect.isEnabled = false
+            self.btnDisconnect.isEnabled = true
+            self.btnGetRoomList.isEnabled = true
+            self.btnCreate.isEnabled = false
+            self.btnJoin.isEnabled = false
+            self.btnLeave.isEnabled = false
+            self.btnSend.isEnabled = true
         }
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        if let text: String = textField.text {
-            if text.count > 0 {
-#if true
-            self.grpcManager.sendMessageToChatRoom(message: text)
-#else
-                self.sendMessageToChatRoom(message: text)
-#endif
-            }
+        if textField == self.tfRoomID {
+            self.btnCreate.isEnabled = true
+            self.btnJoin.isEnabled = true
+        } else if textField == self.tfMessage {
+            self.sendButtonWasPressed(self.btnSend!)
         }
         
         return true
     }
     
-    @IBAction func joinButtonWasPressed(_ sender: Any) {
-#if true
-    self.grpcManager.joinToChatRoom(chatRoom: self.chatRoom, userID: "2", userName: "레이니2") { [weak self] (response) in
-        print(response)
-        
-        if let weakSelf = self {
-            DispatchQueue.main.async {
-                if response.command == .chat {
-                    weakSelf.tfMessage.text = ""
-                    weakSelf.view.endEditing(true)
-                } else {
-                    weakSelf.tfMessage.isHidden = false
+    @IBAction func connectButtonWasPressed(_ sender: Any) {
+        self.grpcManager.connet(host: nil, port: nil) { (response) in
+            print(#function, response)
+        } state: { [weak self] (state) in
+            print(#function, state)
+            
+            if let weakSelf = self {
+                DispatchQueue.main.async {
+                    weakSelf.changeStateButton()
+                }
+            }
+        } error: { (error) in
+            print(#function, error)
+        }
+
+        self.grpcManager.connectChatClientStreamService { [weak self] (response) in
+            if let weakSelf = self {
+                DispatchQueue.main.async {
+                    weakSelf.changeStateButton()
+                    
+                    if response.command == .chat {
+                        weakSelf.tfMessage.text = ""
+                        weakSelf.view.endEditing(true)
+                    } else {
+
+                    }
                 }
             }
         }
     }
-#else
-        self.joinToChatRoom()
-#endif
+    
+    @IBAction func disconnectButtonWasPressed(_ sender: Any) {
+        self.grpcManager.disconnect()
+        self.changeStateButton()
+    }
+    
+    @IBAction func getRoomListButtonWasPressed(_ sender: Any) {
+        if let response: Com_Ncsoft_Aiss_Chat_Paige_V1_GetAllChatRoomResponse = self.grpcManager.getChatRoomList() {
+            if response.chatRooms.count > 0 {
+                if let selectChatRoom: Com_Ncsoft_Aiss_Chat_Paige_V1_ChatRoom = response.chatRooms.first {
+                    self.chatRoom = selectChatRoom
+                    self.btnCreate.isEnabled = true
+                    self.btnJoin.isEnabled = true
+                    
+                    let alert = UIAlertController(title: "방 목록", message: "\(response)", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "확인", style: UIAlertAction.Style.cancel, handler: { action in
+                        
+                    }))
+                    
+                    self.present(alert, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    @IBAction func createButtonWasPressed(_ sender: Any) {
+        self.grpcManager.createChatRoom(createRoomID: "1")
+    }
+    
+    @IBAction func joinButtonWasPressed(_ sender: Any) {
+        self.grpcManager.joinChatRoom(chatRoom: self.chatRoom, userID: "1", userName: "레이니")
     }
     
     @IBAction func leaveButtonWasPressed(_ sender: Any) {
         self.grpcManager.leaveChatRoom()
+        self.changeStateButton()
     }
     
+    @IBAction func sendButtonWasPressed(_ sender: Any) {
+        if let text: String = self.tfMessage.text {
+            if text.count > 0 {
+                self.grpcManager.sendMessageToChatRoom(message: text)
+            }
+        }
+    }
 }
 
--- gRPC_Client/gRPCManager.swift
+++ gRPC_Client/gRPCManager.swift
@@ -17,29 +17,60 @@ import Logging
 
 class gRPCManager: NSObject, ClientErrorDelegate, ConnectivityStateDelegate {
     
+    typealias CurrentState = (ConnectivityState) -> Void
+    typealias Failure = (Error) -> Void
+    typealias Success = (AnyObject) -> Void
+    
+    private var currentStateHandler: CurrentState?
+    private var errorHandler: Failure?
+    private var successHandler: Success?
+    
     let hostName: String = "172.19.136.161"
-    let port: Int = 31105
+    let portNumber: Int = 31105
+    var connection: ClientConnection?
     var chatRoom: Com_Ncsoft_Aiss_Chat_Paige_V1_ChatRoom = Com_Ncsoft_Aiss_Chat_Paige_V1_ChatRoom()
-    var chatRoomServiceclient: Com_Ncsoft_Aiss_Chat_Paige_V1_ChatRoomServiceClient?
-    var chatStreamServiceClientProtocol: Com_Ncsoft_Aiss_Chat_Paige_V1_ChatStreamServiceClientProtocol?
+    var chatRoomServiceClient: Com_Ncsoft_Aiss_Chat_Paige_V1_ChatRoomServiceClient?
+    var chatStreamServiceClient: Com_Ncsoft_Aiss_Chat_Paige_V1_ChatStreamServiceClientProtocol?
     var streamRequest = Com_Ncsoft_Aiss_Chat_Paige_V1_ChatStreamRequest()
     var bidirectionalStreamingCall: BidirectionalStreamingCall<Com_Ncsoft_Aiss_Chat_Paige_V1_ChatStreamRequest, Com_Ncsoft_Aiss_Chat_Paige_V1_ChatStreamResponse>?
     
     override init() {
-        let configuration = ClientConnection.Configuration(target: .hostAndPort(hostName, port), eventLoopGroup: MultiThreadedEventLoopGroup(numberOfThreads: 1))
-        self.chatRoomServiceclient = Com_Ncsoft_Aiss_Chat_Paige_V1_ChatRoomServiceClient(channel: ClientConnection(configuration: configuration))
-        self.chatStreamServiceClientProtocol = Com_Ncsoft_Aiss_Chat_Paige_V1_ChatStreamServiceClient(channel: ClientConnection(configuration: configuration))
-        
         super.init()
+    }
+    
+    func connet(host: String?, port: Int?, success: @escaping Success, state: @escaping CurrentState, error: @escaping Failure) {
+        let group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+        var configuration = ClientConnection.Configuration(target: .hostAndPort(host ?? self.hostName, port ?? self.portNumber), eventLoopGroup: group, tls: nil, connectionBackoff: ConnectionBackoff(initialBackoff: 1, maximumBackoff: 60, multiplier: 1, jitter: 1, minimumConnectionTimeout: 5))
+//        var configuration = ClientConnection.Configuration(target: .hostAndPort(host ?? self.hostName, port ?? self.portNumber), eventLoopGroup: group)
+
+        configuration.errorDelegate = self
+        configuration.connectivityStateDelegate = self
+        
+        self.successHandler = success
+        self.currentStateHandler = state
+        self.errorHandler = error
+        
+        // connection 시작, 클라이언트 생성
+        self.connection = ClientConnection.init(configuration: configuration)
+        self.chatRoomServiceClient = Com_Ncsoft_Aiss_Chat_Paige_V1_ChatRoomServiceClient(channel: self.connection!)
+        
+    }
+    
+    func disconnect() {
+        if let channel: ClientConnection = self.connection {
+            do {
+                try! channel.close().wait()
+            }
+        }
     }
     
     func getChatRoomList() -> Com_Ncsoft_Aiss_Chat_Paige_V1_GetAllChatRoomResponse? {
         do {
             let request = Com_Ncsoft_Aiss_Chat_Paige_V1_GetAllChatRoomRequest()
             
-            if let chatRoomServiceClient: Com_Ncsoft_Aiss_Chat_Paige_V1_ChatRoomServiceClient = self.chatRoomServiceclient {
+            if let chatRoomServiceClient: Com_Ncsoft_Aiss_Chat_Paige_V1_ChatRoomServiceClient = self.chatRoomServiceClient {
                 let response = try chatRoomServiceClient.getAll(request).response.wait()
-
+                
                 print(response)
                 
                 return response
@@ -51,19 +82,66 @@ class gRPCManager: NSObject, ClientErrorDelegate, ConnectivityStateDelegate {
         return nil
     }
     
-    func joinToChatRoom(chatRoom: Com_Ncsoft_Aiss_Chat_Paige_V1_ChatRoom, userID: String, userName: String, completion: @escaping (_ result: Com_Ncsoft_Aiss_Chat_Paige_V1_ChatStreamResponse) -> Void) {
-        self.streamRequest.chatRoomID = chatRoom.id
+    func connectChatClientStreamService(completion: @escaping (_ result: Com_Ncsoft_Aiss_Chat_Paige_V1_ChatStreamResponse) -> Void) {
+        if let channel: ClientConnection = self.connection {
+            self.chatStreamServiceClient = Com_Ncsoft_Aiss_Chat_Paige_V1_ChatStreamServiceClient(channel: channel)
+            
+            if let client: Com_Ncsoft_Aiss_Chat_Paige_V1_ChatStreamServiceClientProtocol = self.chatStreamServiceClient {
+                self.bidirectionalStreamingCall = client.send(handler: { (response) in
+                    print(response)
+                    
+                    completion(response)
+                })
+            }
+            
+            if let call: BidirectionalStreamingCall<Com_Ncsoft_Aiss_Chat_Paige_V1_ChatStreamRequest, Com_Ncsoft_Aiss_Chat_Paige_V1_ChatStreamResponse> = self.bidirectionalStreamingCall {
+                call.status.whenSuccess({ (status) in
+                    print(#function, status)
+                })
+                
+                call.status.whenFailure({ (error) in
+                    print(#function, error)
+                })
+                
+                call.status.whenComplete({ (result) in
+                    print(#function, result)
+                    
+                    switch result {
+                    case let .success(value):
+                        print(value)
+                        
+                    case let .failure(error):   
+                        print(error)
+                        
+                    }
+                })
+            }
+        }
+    }
+    
+    func createChatRoom (createRoomID: String, createRoomTitle: String = "우경이의 방") {
+        var createChatRequest = Com_Ncsoft_Aiss_Chat_Paige_V1_CreateChatRoomRequest()
+        
+        createChatRequest.chatRoomID = createRoomID
+        createChatRequest.chatRoomTitle = createRoomTitle
+        
+        if let client: Com_Ncsoft_Aiss_Chat_Paige_V1_ChatRoomServiceClient = self.chatRoomServiceClient {
+            client.create(createChatRequest).status.whenComplete { (result) in
+                switch result {
+                case let .success(value):
+                    print(value)
+                case let .failure(error):
+                    print(error)
+                }
+            }
+        }
+    }
+    
+    func joinChatRoom(chatRoom: Com_Ncsoft_Aiss_Chat_Paige_V1_ChatRoom, userID: String, userName: String) {
+        self.streamRequest.chatRoomID = "````test4````" // chatRoom.id
         self.streamRequest.command = .join
         self.streamRequest.userID = userID
         self.streamRequest.userName = userName
-        
-        if let client: Com_Ncsoft_Aiss_Chat_Paige_V1_ChatStreamServiceClientProtocol = self.chatStreamServiceClientProtocol {
-            self.bidirectionalStreamingCall = client.send(handler: { (response) in
-                print(response)
-
-                completion(response)
-            })
-        }
         
         if let call: BidirectionalStreamingCall<Com_Ncsoft_Aiss_Chat_Paige_V1_ChatStreamRequest, Com_Ncsoft_Aiss_Chat_Paige_V1_ChatStreamResponse>
             = self.bidirectionalStreamingCall {
@@ -93,11 +171,19 @@ class gRPCManager: NSObject, ClientErrorDelegate, ConnectivityStateDelegate {
     //MARK: - ClientErrorDelegate methods
     func didCatchError(_ error: Error, logger: Logger, file: StaticString, line: Int) {
         print(error.localizedDescription)
+        
+        if let fail: Failure = self.errorHandler {
+            fail(error)
+        }
     }
     
     //MARK: - ConnectivityStateDelegate methods
     func connectivityStateDidChange(from oldState: ConnectivityState, to newState: ConnectivityState) {
-        print(oldState, newState)
+        print(#function, oldState, newState)
+        
+        if let state: CurrentState = self.currentStateHandler {
+            state(newState)
+        }
     }
     
     func connectionStartedQuiescing() {
